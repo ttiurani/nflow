5e8c7596c1c4bee0b59bb963c0292e532bef2213
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2017-08-09 10:06:02 +0300

    In preparation for spring-webflux, split nflow-rest-api into two parts. nflow-rest-api-core contains classes not dependent on REST API implementation tech, and nflow-rest-api-jax-rs contains the JAX-RS implementation.

---
+++ nflow-rest-api-spring-web/src/main/java/io/nflow/rest/v1/springweb/WorkflowExecutorResource.java
@@ -0,0 +1,40 @@
+package io.nflow.rest.v1.springweb;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.nflow.engine.service.WorkflowExecutorService;
+import io.nflow.rest.v1.converter.ListWorkflowExecutorConverter;
+import io.nflow.rest.v1.msg.ListWorkflowExecutorResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping(value = "/nflow/v1/workflow-executor", produces = "application/json")
+@Api("nFlow workflow executor management")
+@Component
+public class WorkflowExecutorResource {
+
+  private final WorkflowExecutorService workflowExecutors;
+  private final ListWorkflowExecutorConverter converter;
+
+  @Autowired
+  public WorkflowExecutorResource(WorkflowExecutorService workflowExecutors, ListWorkflowExecutorConverter converter) {
+    this.workflowExecutors = workflowExecutors;
+    this.converter = converter;
+  }
+
+  @GetMapping
+  @ApiOperation(value = "List workflow executors", response = ListWorkflowExecutorResponse.class, responseContainer = "List")
+  public Collection<ListWorkflowExecutorResponse> listWorkflowExecutors() {
+    return workflowExecutors.getWorkflowExecutors().stream()
+        .map(executor -> this.converter.convert(executor))
+        .collect(Collectors.toList());
+  }
+}
