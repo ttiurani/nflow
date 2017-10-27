ee1a9c055374d567d1787bbfc569a9472cd95c79
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2017-10-27 14:14:13 +0300

    Remove requirement for spring-boot

--- nflow-netty/src/main/java/io/nflow/netty/StartNFlow.java
+++ nflow-netty/src/main/java/io/nflow/netty/StartNFlow.java
@@ -1,50 +1,89 @@
 package io.nflow.netty;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.DefaultApplicationArguments;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.core.io.support.ResourcePropertySource;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.config.DelegatingWebFluxConfiguration;
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
 
 import io.nflow.netty.config.NflowNettyConfiguration;
 import io.nflow.server.spring.NflowStandardEnvironment;
+import reactor.ipc.netty.http.server.HttpServer;
 
 public class StartNFlow {
 
-  private final Class<?> springBootMainClass;
+  public static final String DEFAULT_SERVER_CONFIGURATION = "application.properties";
+  public static final String DEFAULT_SERVER_IP = "0.0.0.0";
+  public static final Integer DEFAULT_SERVER_PORT = 12012;
 
-  public StartNFlow(Class<?> springBootMainClass) {
-    this.springBootMainClass = springBootMainClass;
+  private final Class<?> springMainClass;
+
+  public StartNFlow(Class<?> springMainClass) {
+    this.springMainClass = springMainClass;
   }
 
-  public ApplicationContext startNetty(final boolean createDatabase, final boolean autoStartNflow) {
-    return this.startNetty(new String[]{}, createDatabase, autoStartNflow);
+  public ApplicationContext startNetty(final boolean createDatabase, final boolean autoStartNflow) throws IOException {
+    return this.startNetty(new String[] {}, createDatabase, autoStartNflow, DEFAULT_SERVER_CONFIGURATION);
   }
 
-  public ApplicationContext startNetty(final String[] args, final boolean createDatabase, final boolean autoStartNflow) {
+  public ApplicationContext startNetty(final boolean createDatabase, final boolean autoStartNflow,
+      final String mainConfigurationClasspath) throws IOException {
+    return this.startNetty(new String[] {}, createDatabase, autoStartNflow, mainConfigurationClasspath);
+  }
+
+  public ApplicationContext startNetty(final String[] args, final boolean createDatabase,
+      final boolean autoStartNflow) throws IOException {
+    return this.startNetty(args, createDatabase, autoStartNflow, DEFAULT_SERVER_CONFIGURATION);
+  }
+
+  public ApplicationContext startNetty(final String[] args, final boolean createDatabase,
+      final boolean autoStartNflow, final String mainConfigurationClasspath) throws IOException {
     final String[] customArgs = Arrays.copyOf(args, args.length + 2);
     customArgs[customArgs.length-2] = "--nflow.db.create_on_startup=" + createDatabase;
     customArgs[customArgs.length-1] = "--nflow.autostart=" + autoStartNflow;
-    return this.startNetty(customArgs);
+    return this.startNetty(customArgs, mainConfigurationClasspath);
   }
 
-  public ApplicationContext startNetty(final String[] args) {
-    final SpringApplication application = new SpringApplication(springBootMainClass, NflowNettyConfiguration.class);
-    final ApplicationArguments arguments = new DefaultApplicationArguments(args);
+  public ApplicationContext startNetty(final String[] args, final String mainConfigurationClasspath) throws IOException {
     final Map<String, Object> argsMap = new HashMap<>();
-    arguments.getOptionNames().forEach(optionName -> {
-      argsMap.put(optionName, arguments.getOptionValues(optionName));
-    });
-    arguments.getNonOptionArgs().forEach(arg -> {
-      argsMap.put(arg, null);
-    });
-    final ConfigurableEnvironment env = new NflowStandardEnvironment(argsMap);
-    application.setEnvironment(env);
-    return application.run(args);
-  }
 
+    // Add optional arguments to map, using Spring's helper class
+    final SimpleCommandLinePropertySource arguments = new SimpleCommandLinePropertySource(args);
+    Arrays.asList(arguments.getPropertyNames()).forEach(optionName -> {
+      argsMap.put(optionName, arguments.getProperty(optionName));
+    });
+
+    // Add also properties that are not optional, i.e. don't start with "--"
+    Arrays.asList(args).stream().filter(value -> !value.startsWith("--")).forEach(value -> argsMap.put(value, null));
+
+    // Create context
+    final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+    final NflowStandardEnvironment env = new NflowStandardEnvironment(argsMap);
+    final ResourcePropertySource mainConfiguration = new ResourcePropertySource(mainConfigurationClasspath);
+    env.getPropertySources().addLast(mainConfiguration);
+    context.setEnvironment(env);
+    context.register(DelegatingWebFluxConfiguration.class, NflowNettyConfiguration.class, this.springMainClass);
+    context.refresh();
+
+    // Start netty
+    final HttpHandler handler = WebHttpHandlerBuilder.applicationContext(context).build();
+    final ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(handler);
+    HttpServer.create(
+        mainConfiguration.containsProperty("server.ip") ? (String) mainConfiguration.getProperty("server.ip")
+            : DEFAULT_SERVER_IP,
+        mainConfiguration.containsProperty("server.port") ? Integer.valueOf((String) mainConfiguration.getProperty("server.port"))
+            : DEFAULT_SERVER_PORT)
+        .newHandler(adapter)
+        .block();
+
+    return context;
+  }
 }
--- nflow-netty/src/main/java/io/nflow/netty/config/NflowNettyConfiguration.java
+++ nflow-netty/src/main/java/io/nflow/netty/config/NflowNettyConfiguration.java
@@ -2,27 +2,37 @@ package io.nflow.netty.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.reactive.DispatcherHandler;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 import io.nflow.engine.internal.config.NFlow;
 import io.nflow.rest.config.RestConfiguration;
 
 @Configuration
+@ComponentScan("ios.nflow.rest.v1.springweb")
 @Import(value = { RestConfiguration.class })
 @EnableTransactionManagement
-@ComponentScans({@ComponentScan("io.nflow.engine"), @ComponentScan("io.nflow.rest.v1.springweb")})
+@EnableWebFlux
+@PropertySource("application.properties")
 public class NflowNettyConfiguration {
 
   @Bean
-  public PlatformTransactionManager transactionManager(@NFlow DataSource nflowDataSource)  {
+  public PlatformTransactionManager transactionManager(@NFlow DataSource nflowDataSource) {
     return new DataSourceTransactionManager(nflowDataSource);
+  }
+
+  @Bean
+  public DispatcherHandler webHandler(ApplicationContext context) {
+    return new DispatcherHandler(context);
   }
 
 }
